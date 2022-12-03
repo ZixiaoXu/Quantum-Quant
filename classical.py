c391817d9e18fa8cae63d9988cbfc7d4cadf4ace
Alex Li <7alex7li@gmail.com>
2022-12-03 14:33:58 -0500

    Add thesholding for numerical stability

--- classical.py
+++ classical.py
@@ -3,7 +3,6 @@ import numpy as np
 import pandas as pd
 from tqdm import tqdm
 import zipfile
-SEQUENCE_LENGTH = 10
 def get_past(df):
     return df.loc[df['Date'] <= '2019-03-31']
 
@@ -12,19 +11,19 @@ def get_past(df):
 def get_future(df):
   return df.loc[df['Date'] >= '2019-03-01']
 
-def split_data(all_dfs, n_stocks):
+def split_data(all_dfs, n_stocks, n_stocks_val):
   # There is one test/eval data point for each train data point
-  val_count = n_stocks // 2
+  np.random.seed(1)
   perm = np.random.permutation(n_stocks)
-  val_stock_inds = perm[:val_count]
-  test_stock_inds = perm[val_count:]
+  val_stock_inds = perm[n_stocks_val: 2 * n_stocks_val]
+  test_stock_inds = perm[:n_stocks_val]
 
-  x_train = [get_past(df) for df in all_dfs]
+  x_train = [get_past(df) for df in all_dfs[perm[2*n_stocks_val:]]]
   x_val = [df for df in all_dfs[val_stock_inds]]
   x_test = [df for df in all_dfs[test_stock_inds]]
   return x_train, x_val, x_test
 
-def normalize(df):
+def normalize(df, sequence_length):
   volume = df['Volume'][1:]
   other = df.drop('Volume', axis=1)
   # Normalize everything but volume
@@ -34,7 +33,6 @@ def normalize(df):
   normalized = normalized / (normalized.std(axis=0)+ 1e-3)
 
   if normalized.isna().values.any():
-    print("FAIL2")
     return None  # idk why this can happen
 
   # Normalize volume
@@ -43,7 +41,7 @@ def normalize(df):
   normalized['Volume'] = volume
 
   assert not normalized.isna().values.any()
-  SL = SEQUENCE_LENGTH + 1
+  SL = sequence_length + 1
   cut_off = normalized.tail(SL)
   rows_to_prepend = SL - cut_off.shape[0]
   if rows_to_prepend != 0:
@@ -53,8 +51,8 @@ def normalize(df):
     df_prepend = pd.DataFrame(base, columns=df.columns)
     cut_off = df_prepend.append(cut_off, ignore_index=True)
   return cut_off
-def preprocess_data(raw_data):
-  normalized = [normalize(df.drop("Date", axis=1)) for df in raw_data]
+def preprocess_data(raw_data, sequence_length):
+  normalized = [normalize(df.drop("Date", axis=1), sequence_length) for df in raw_data]
   filtered = [df for df in normalized if df is not None]
   # x[1].close is y[0]
   x = [df[:-1] for df in filtered]
@@ -75,10 +73,9 @@ def extract_zip():
     with zipfile.ZipFile(path_to_zip_file, 'r') as zip_ref:
         zip_ref.extractall("data/archive")
 
-def get_data():
+def get_data(sequence_length, n_stocks_train=1000, n_stocks_val=50):
     np.random.seed(0)
-    print("Loading stocks")
-    TOTAL_STOCKS_ORIGINAL = min(len(os.listdir('data/archive/stocks')), 50)
+    TOTAL_STOCKS_ORIGINAL = min(len(os.listdir('data/archive/stocks')), n_stocks_train + 2 * n_stocks_val)
     N_STOCKS = TOTAL_STOCKS_ORIGINAL
     stock_paths = np.random.permutation(os.listdir('data/archive/stocks'))[:N_STOCKS]
 
@@ -88,34 +85,36 @@ def get_data():
     all_dfs = np.array(all_dfs, dtype=object)
     print("Preprocessing Data")
 
-    raw_train_data, raw_val_data, raw_test_data = split_data(all_dfs, N_STOCKS)
+    raw_train_data, raw_val_data, raw_test_data = split_data(all_dfs, N_STOCKS, n_stocks_val)
     for df in raw_train_data:
       assert isinstance(df, pd.DataFrame)
 
 
-    x_train, y_train = preprocess_data(raw_train_data)
-    x_val, y_val = preprocess_data(raw_val_data)
-    x_test, y_test = preprocess_data(raw_test_data)
+    x_train, y_train = preprocess_data(raw_train_data, sequence_length)
+    x_val, y_val = preprocess_data(raw_val_data, sequence_length)
+    x_test, y_test = preprocess_data(raw_test_data, sequence_length)
 
-    assert x_train[17].shape == (SEQUENCE_LENGTH, 6)
-    assert y_train[17].shape == (SEQUENCE_LENGTH,)
+    assert x_train[17].shape == (sequence_length, 6)
+    assert y_train[17].shape == (sequence_length,)
     
         # Shapes correct
     assert(x_train[0].shape[1] == 6)
     assert(x_train[17].shape[0] == y_train[17].shape[0])
     # Each output is passed to next input
-    assert x_train[0][1, 3] == y_train[0][0]
-    assert x_train[0][2, 3] == y_train[0][1]
-    assert x_train[0][3, 3] == y_train[0][2]
+    # assert x_train[0][1, 3] == y_train[0][0]
+    # assert x_train[0][2, 3] == y_train[0][1]
+    # assert x_train[0][3, 3] == y_train[0][2]
     assert not np.isnan(x_train[0]).any()
     return x_train, y_train, x_val, y_val, x_test, y_test
 
 def bounded_map(x):
-    return np.tanh(x) * np.pi / 2
+    # We clip for some numerical stability, but it may make some
+    # answers wrong/ easier to predict.
+    return np.arctan(np.clip(x, -5, 5))
 
 def inv_bounded_map(x):
-    return np.arctanh(x * 2 / np.pi)
+    return np.tan(x)
 
 if __name__ == "__main__":
     assert abs(inv_bounded_map(bounded_map(.8543)) - .8543) < 1e-6
-    get_data()
+    assert abs(inv_bounded_map(bounded_map(-.8543)) + .8543) < 1e-6
