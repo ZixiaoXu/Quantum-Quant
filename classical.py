0c4c9df8e4ec1889e7ebac8abaee99304191ef3f
Alex Li <7alex7li@gmail.com>
2022-12-04 18:20:19 -0500

    Better graphs'

--- classical.py
+++ classical.py
@@ -12,19 +12,18 @@ from tensorflow.python.keras.optimizers import adam_v2
 
 def get_past(df):
     # Get old but not that old data
-    return df.loc[(df['Date'] <= '2019-05-31') & (df['Date'] >= '2019-04-01')]
+    return df.loc[('2019-04-01' <= df['Date']) & (df['Date'] <= '2019-05-01')]
 
 def get_future(df):
-  return df.loc[(df['Date'] >= '2019-06-01') & (df['Date'] >= '2019-05-01')]
+  return df.loc[('2019-05-01' <= df['Date']) & (df['Date'] >= '2019-06-01')]
 
 def split_data(all_dfs, n_stocks, n_stocks_val):
   # There is one test/eval data point for each train data point
-  np.random.seed(1)
   perm = np.random.permutation(n_stocks)
-  val_stock_inds = perm[n_stocks_val: 2 * n_stocks_val]
-  test_stock_inds = perm[:n_stocks_val]
+  val_stock_inds = perm[:n_stocks_val]
+  test_stock_inds = perm[n_stocks_val: 2 * n_stocks_val]
 
-  x_train = [get_past(df) for df in all_dfs]
+  x_train = [get_future(df) for df in all_dfs[n_stocks_val * 2:]]
   x_val = [get_future(df) for df in all_dfs[val_stock_inds]]
   x_test = [get_future(df) for df in all_dfs[test_stock_inds]]
   return x_train, x_val, x_test
@@ -82,9 +81,9 @@ def extract_zip():
     with zipfile.ZipFile(path_to_zip_file, 'r') as zip_ref:
         zip_ref.extractall("data/archive")
 
-def get_data(sequence_length, n_stocks_train=1000, n_stocks_val=50):
-    np.random.seed(0)
-    TOTAL_STOCKS_ORIGINAL = min(len(os.listdir('data/archive/stocks')), max(n_stocks_train, 2 * n_stocks_val))
+def get_data(sequence_length, n_stocks_train=1000, n_stocks_val=50, get_close_val_only=True):
+    np.random.seed(1)
+    TOTAL_STOCKS_ORIGINAL = min(len(os.listdir('data/archive/stocks')), n_stocks_train + 2 * n_stocks_val)
     N_STOCKS = TOTAL_STOCKS_ORIGINAL
     stock_paths = np.random.permutation(os.listdir('data/archive/stocks'))[:N_STOCKS]
 
@@ -114,6 +113,13 @@ def get_data(sequence_length, n_stocks_train=1000, n_stocks_val=50):
     # assert x_train[0][2, 3] == y_train[0][1]
     # assert x_train[0][3, 3] == y_train[0][2]
     assert not np.isnan(x_train[0]).any()
+    if get_close_val_only:
+        close_ind = 3
+        def get_close_only(x_data):
+            return [x_datapoint[:, close_ind] for x_datapoint in x_data]
+        x_train = get_close_only(x_train)
+        x_val = get_close_only(x_val)
+        x_test = get_close_only(x_test)
     return x_train, y_train, x_val, y_val, x_test, y_test
 
 def bounded_map(x):
@@ -155,13 +161,49 @@ def train_model(model: Model, train_dataset: tf.data.Dataset, val_dataset: tf.da
         callbacks=[tensorboard_callback]
         )
     return model
+
+def contant_baseline(y_train, y_val, y_test):
+    pred_value = 0
+    for y in y_train:
+        pred_value += y[-1]
+    pred_value /= len(y_train)
+    print(pred_value)
+
+    for name, test_set in ("train", y_train), ("Val", y_val), ("Test", y_test):
+        losses = []
+        print(f"N datapoints: {len(test_set)}")
+        for y in test_set:
+            losses.append((y[-1] - pred_value) ** 2)
+        print(f"{name} Loss: {sum(losses) / len(losses)}")
+
+def tf_baseline(num_hidden, sequence_length, x_train, y_train, x_val, y_val, x_test, y_test):
+    num_hidden = 64
+    model_tf = make_lstm(sequence_length, 1, num_hidden)
+    # Just use the last element of the time series
+    y_train = [y[-1] for y in  y_train]
+    y_val = [y[-1] for y in  y_val]
+    y_test = [y[-1] for y in  y_test]
+    train_tf = tf.data.Dataset.from_tensor_slices((x_train, y_train))
+    val_tf = tf.data.Dataset.from_tensor_slices((x_val, y_val))
+    model_tf = train_model(model_tf, train_tf, val_tf)
+    test_tf = tf.data.Dataset.from_tensor_slices((x_test, y_test))
+    print("Train result:")
+    model_tf.evaluate(train_tf.batch(32))
+    print("Val result:")
+    model_tf.evaluate(val_tf.batch(32))
+    print("Test result:")
+    model_tf.evaluate(test_tf.batch(32))
+    
 if __name__ == "__main__":
     assert abs(inv_bounded_map(bounded_map(.8543)) - .8543) < 1e-6
     assert abs(inv_bounded_map(bounded_map(-.8543)) + .8543) < 1e-6
-    x_train, y_train, x_val, y_val, x_test, y_test = get_data(5, 500, 500)
+    sequence_length = 5
+    x_train, y_train, x_val, y_val, x_test, y_test = get_data(sequence_length, 100, 64)
     print(np.mean(y_train))
     print(np.mean(y_val))
     print(np.mean(y_test))
     print(np.std(y_train))
     print(np.std(y_val))
     print(np.std(y_test))
+    contant_baseline(y_train, y_val, y_test)
+    tf_baseline(64, sequence_length,  x_train, y_train, x_val, y_val, x_test, y_test)
